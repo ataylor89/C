4ef644d4b9d832e8b496fd5b31014e1e6d6587a9
Andrew <rolyata47@gmail.com>
2021-09-08 11:42:33 -0500

    Creates a GtkApplication with a callback function

---
+++ gtk1.c
@@ -0,0 +1,16 @@
+#include <gtk/gtk.h>
+
+static void activate(GApplication *app, gpointer *data) {
+	g_print("GtkApplication is activated.\n");
+}
+
+int main(int argc, char **argv) {
+	GtkApplication *app;
+	int stat;
+
+	app = gtk_application_new("com.App", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	stat = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
+	return stat;
+}
